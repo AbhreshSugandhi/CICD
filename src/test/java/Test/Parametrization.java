a6af98b6d5472d482f9aa23dec4eceaf67189862
Abhresh <tabhresh@gmail.com>
2020-04-23 17:02:32 +0530

    10th Commit

--- src/test/java/Test/Parametrization.java
+++ src/test/java/Test/Parametrization.java
@@ -23,33 +23,6 @@ public class Parametrization extends BrowserClass {
 		driver.get("http://github.com");
 	}
 
-	@Parameters({"UserName", "Password"})
-	@Test(priority = 2)
-	public void Login(String UName, String Pwd) throws InterruptedException {
-
-		driver.findElement(By.xpath("//*[@href='/login']")).click();
-
-		Thread.sleep(3000);
-
-		driver.findElement(By.id("login_field")).sendKeys(UName);
-
-		driver.findElement(By.id("password")).sendKeys(Pwd);
-
-		driver.findElement(By.name("commit")).click();
-	}
-
-	@Test(priority = 3)
-	public void Logout() throws InterruptedException {
-
-		Thread.sleep(3000);
-
-		driver.findElement(By.xpath("//*[@aria-label='View profile and more' and @aria-haspopup='menu']")).click();
-
-		driver.findElement(
-				By.xpath("//*[@aria-label='View profile and more' and @aria-haspopup='menu']//following::button[15]"))
-				.click();
-
-	}
 
 	@Test(priority = 4)
 	public void QuitBrowser() throws InterruptedException {
